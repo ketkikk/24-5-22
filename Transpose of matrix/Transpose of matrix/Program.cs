0d95ba6aee6450297eb96b9d80df623d7c1a5fc1
ketkikk <kulketki7@gmail.com>
2022-05-25 11:20:25 +0530

    first commit

---
+++ Transpose of matrix/Transpose of matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Transpose_of_matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, i, j;
+            Console.Write("Enter the Order of the Matrix : ");
+            m = Convert.ToInt16(Console.ReadLine());
+            n = Convert.ToInt16(Console.ReadLine());
+            int[,] A = new int[10, 10];
+            Console.Write("\nEnter The Matrix Elements : ");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    A[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("\nMatrix A : ");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(A[i, j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Transpose Matrix : ");
+
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(A[j, i] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
